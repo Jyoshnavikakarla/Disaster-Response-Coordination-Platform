b9a810ca8dc2b24e5526249bdce9aac7d1ed14e6
Jyoshnavikakarla <166583522+Jyoshnavikakarla@users.noreply.github.com>
2025-09-18 08:19:33 +0530

    Update Home.jsx
    made home page more visual appealing using framer motion for smooth animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
     <main className="home">
       <section className="hero">
-        <div className="hero-text">
-          <h1>Disaster Response<br/>Coordination Platform</h1>
-          <p className="tagline">Together, we can save lives and rebuild hope.</p>
-        </div>
+        <motion.div
+          className="hero-text"
+          initial={{ opacity: 0, y: -40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <h1>
+            Disaster Response <br /> Coordination Platform
+          </h1>
+          <p className="tagline">
+            Together, we can save lives and rebuild hope.
+          </p>
+        </motion.div>
       </section>
     </main>
   );
