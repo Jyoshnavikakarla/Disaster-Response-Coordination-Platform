c3740f3717a0e1017a494eac00fd361b8949fcc0
Jyoshnavikakarla <166583522+Jyoshnavikakarla@users.noreply.github.com>
2025-09-18 08:14:01 +0530

    Update main.jsx
    included AppContext in main for using it across the project

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./style.css";
+import { AppProvider } from "./AppContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AppProvider>
       <App />
+    </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
