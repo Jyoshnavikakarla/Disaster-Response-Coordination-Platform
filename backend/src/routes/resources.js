1eeb58c119043601d585f1332edfd40db50483a4
NithyashreeGowda <nithyashreekp99@gmail.com>
2025-10-19 20:48:28 +0530

    Update resources.js

--- backend/src/routes/resources.js
+++ backend/src/routes/resources.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const  authMiddleware  = require('../middlewares/auth');
-const Resource = require('../models/Resource');
+const Resource = require("../models/Resource");
+const { protect } = require("../middlewares/auth");
 
 // Create a resource
-router.post('/', authMiddleware, async (req, res) => {
+router.post("/", protect, async (req, res) => {
   try {
     const { name, location, contact, details, role } = req.body;
     const resource = new Resource({
@@ -13,26 +13,25 @@ router.post('/', authMiddleware, async (req, res) => {
       contact,
       details,
       role,
-      userId: req.user.id, // optional linking
+      userId: req.user.id,
     });
     await resource.save();
-    res.status(201).json({ message: 'Resource created', resource });
+    res.status(201).json({ message: "Resource created", resource });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
 // Get all resources
-router.get('/', authMiddleware, async (req, res) => {
+router.get("/", protect, async (req, res) => {
   try {
     const resources = await Resource.find();
     res.json(resources);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
 module.exports = router;
-
