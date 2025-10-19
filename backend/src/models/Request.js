dd52b1a91684252bf14c38758b26610846262716
Jyoshnavikakarla <166583522+Jyoshnavikakarla@users.noreply.github.com>
2025-10-19 17:12:38 +0530

    Update Request.js

--- backend/src/models/Request.js
+++ backend/src/models/Request.js
@@ -1,16 +1,20 @@
 // src/models/Request.js
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const RequestSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  name: String,
-  location: String,
-  lat: Number,       // optional for map display
-  lng: Number,
-  contact: String,
-  details: String,
-  status: { type: String, default: "pending" }, // pending, completed, in-progress
-  role: String // victim or volunteer
-}, { timestamps: true });
+const RequestSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    name: String,
+    location: String,
+    lat: Number,       // optional for map display
+    lng: Number,
+    contact: String,
+    details: String,
+    status: { type: String, default: "pending" }, // pending, completed, in-progress
+    role: String // victim or volunteer
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Request", RequestSchema);
+const Request = mongoose.model("Request", RequestSchema);
+export default Request;
