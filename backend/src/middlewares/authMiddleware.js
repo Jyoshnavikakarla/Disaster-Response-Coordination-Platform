b22f05c2c89285bb1100c848e1b4f8760f9db406
Jyoshnavikakarla <166583522+Jyoshnavikakarla@users.noreply.github.com>
2025-10-25 23:03:52 +0530

    Update authMiddleware.js

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 // src/middlewares/authMiddleware.js
 const jwt = require("jsonwebtoken");
 
-export const authenticateToken = (req, res, next) => {
+const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ message: "No token provided" });
@@ -12,3 +12,4 @@ export const authenticateToken = (req, res, next) => {
     next();
   });
 };
+module.export = authenticateToken;
