2c6ac1ab542ecfa1d3558a339f6f7260a6e61e60
Jyoshnavikakarla <166583522+Jyoshnavikakarla@users.noreply.github.com>
2025-10-25 21:45:27 +0530

    Update Report.js

--- backend/src/models/Report.js
+++ backend/src/models/Report.js
@@ -1,12 +1,18 @@
-// Report.js
+// src/models/Report.js
 const mongoose = require("mongoose");
 
 const reportSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  status: {
+    type: String,
+    enum: ["Pending", "Ongoing", "Resolved"],
+    default: "Pending",
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Report = mongoose.model('Report', reportSchema);
-module.exports=  Report;
+const Report = mongoose.model("Report", reportSchema);
+module.exports = Report;
