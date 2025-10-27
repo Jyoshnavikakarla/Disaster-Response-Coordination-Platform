901f08f9baa80b0528d66fbe4f4d05ce8dc9f01d
NithyashreeGowda <nithyashreekp99@gmail.com>
2025-10-27 17:26:10 +0530

    Update errorHandler.js

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,8 +1,12 @@
 // errorHandler.js
-export const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(err.status || 500).json({
     success: false,
     message: err.message || 'Server Error'
   });
+};
+
+module.exports = {
+  errorHandler,
 };
